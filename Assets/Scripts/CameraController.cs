4c5bb8f433d08cda7d5addfb92d0d039bf67bb79
Lasha Surguladze <44227081+surlabo@users.noreply.github.com>
2025-04-24 09:10:54 -0700

    UI Update

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,8 +4,10 @@ public class CameraController : MonoBehaviour
 {
     public Transform playerTransform;
 
+
     private void Update()
     {
         transform.position = new Vector3(playerTransform.position.x, 0, transform.position.z);
     }
+
 }
--- Assets/Scripts/StartManuController.cs
+++ Assets/Scripts/StartManuController.cs
@@ -1,12 +1,26 @@
+using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StartManuController : MonoBehaviour
 {
+    public static StartManuController Instance;
+    [SerializeField] private Button startBtn;
     [SerializeField] private GameObject startManuPanel;
+    [SerializeField] private GameObject inGamePanel;
 
 
-    public void DeactivateStartManu()
+
+    private void Start()
+    {
+        startBtn.onClick.AddListener(DeactivateStartManu);
+    }
+
+    private void DeactivateStartManu()
     {
         startManuPanel.SetActive(false);
+        inGamePanel.SetActive(true);
     }
+ 
 }
+
